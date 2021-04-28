f62ca62dcdbceeb7c725966afcbdd385eeab8989
Jerudiel <jerudiel.ortiz@gmail.com>
2021-04-28 13:16:32 -0500

    Modificar formulas para mejor resolución pwm

--- utilidades/formulas.cpp
+++ utilidades/formulas.cpp
@@ -71,7 +71,7 @@ int Formulas::obtener_valor_pwm(float flujo){
 
 int Formulas::obtener_valor_pwm_aire(int fio2){
     try {
-        int pwm_aire = 0;
+        float pwm_aire = 0;
         if(fio2 < porce_cambio){
             pwm_aire = pwm_max;
         }
@@ -85,10 +85,18 @@ int Formulas::obtener_valor_pwm_aire(int fio2){
             else{
                 //pwm_aire = 300 + (4*(100-porcentaje));
                 //pwm_aire = m_aire - ((porcentaje - porce_cambio)*((m_aire - li_aire)/(100-porce_cambio)));
-                pwm_aire = ls_aire + ((fio2 - porce_cambio)*((li_aire - ls_aire)/(100 - porce_cambio)));
+
+                /*float temp1 = ls_aire;
+                float temp2 = fio2 - porce_cambio;
+                float temp3 = li_aire - ls_aire;
+                float temp4 = 100 - porce_cambio;
+                float temp5 = (temp3/temp4);
+                pwm_aire =  temp1 + (temp2 * temp5);*/
+
+                pwm_aire = ls_aire + (1.0*(fio2 - porce_cambio)*((li_aire - ls_aire)/(100.0 - porce_cambio)));
             }
         }
-        return pwm_aire;
+        return static_cast<int>(pwm_aire);
     }  catch (std::exception &e) {
         qWarning("Error %s desde la funcion %s", e.what(), Q_FUNC_INFO );
         return 0;
@@ -97,7 +105,7 @@ int Formulas::obtener_valor_pwm_aire(int fio2){
 
 int Formulas::obtener_valor_pwm_oxigeno(int fio2){
     try {
-        int pwm_oxigeno = 0;
+        float pwm_oxigeno = 0;
         if(fio2 < porce_cambio){
             if(fio2 == 21){
                 pwm_oxigeno = pwm_min;
@@ -106,7 +114,24 @@ int Formulas::obtener_valor_pwm_oxigeno(int fio2){
                 //float cociente = 30/21.0;
                 //pwm_oxigeno = ((porcentaje - 22) * cociente) + 180;
                 //pwm_oxigeno = of_o2 + (fio2 * ((pwm_max - of_o2)/(porce_cambio)));
-                pwm_oxigeno = li_o2 + ((fio2 - 21)*((ls_o2 - li_o2)/(porce_cambio - 21)));
+                //qDebug() << "Entra a obtener_valor_pwm_oxigeno < porce_cambio";
+                //qDebug() << "li_o2: " + QString::number(li_o2);
+                //qDebug() << "ls_o2: " + QString::number(ls_o2);
+                //qDebug() << "fio2: " + QString::number(fio2);
+                //qDebug() << "porce_cambio: " + QString::number(porce_cambio);
+                //float temp1 = li_o2;
+                //qDebug() << "1 - li_o2: " + QString::number(temp1);
+                //float temp2 = fio2 - 21;
+                //qDebug() << "2 - fio2 - 21: " + QString::number(temp2);
+                //float temp3 = ls_o2 - li_o2;
+                //qDebug() << "3 - ls_o2 - li_o2: " + QString::number(temp3);
+                //float temp4 = porce_cambio - 21;
+                //qDebug() << "4 - porce_cambio - 21: " + QString::number(temp4,'f',2);
+                //float temp5 = (temp3/temp4);
+                //qDebug() << "5 - (ls_o2 - li_o2)/(porce_cambio - 21): " + QString::number(temp5,'f',2);
+                pwm_oxigeno = li_o2 + (1.0*(fio2 - 21)*((ls_o2 - li_o2)/(porce_cambio - 21.0)));
+                ////pwm_oxigeno =  temp1 + (temp2 * temp5);
+                //qDebug() << "pwm_oxigeno: " + QString::number(pwm_oxigeno,'f',2);
             }
         }
         else if(fio2 == porce_cambio){
@@ -116,7 +141,7 @@ int Formulas::obtener_valor_pwm_oxigeno(int fio2){
             //float cociente = 16/21.0;
             pwm_oxigeno = pwm_max; //((porcentaje - 22) * cociente) + 225;
         }
-        return pwm_oxigeno;
+        return static_cast<int>(pwm_oxigeno);
     }  catch (std::exception &e) {
         qWarning("Error %s desde la funcion %s", e.what(), Q_FUNC_INFO );
         return 0;
